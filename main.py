1e9481c154aa6690dc444aec1de152a8277d0303
schoolvanmorgen <jelles1997@gmail.com>
2026-05-16 17:27:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from fastapi.responses import FileResponse, StreamingResponse, JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from fastapi.staticfiles import StaticFiles
-from pydantic import BaseModel, field_validator
+from pydantic import BaseModel, Field, validator
 from typing import Optional, List
 from datetime import datetime, timezone
 import httpx
